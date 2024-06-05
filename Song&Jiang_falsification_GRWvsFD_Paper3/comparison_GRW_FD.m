7fa43f3a10d70e0e1c5c77044de66fa683672800
PMFlow <48481148+PMFlow@users.noreply.github.com>
2024-06-05 02:27:36 +0200

    Add files via upload

---
+++ Song&Jiang_falsification_GRWvsFD_Paper3/comparison_GRW_FD.m
@@ -0,0 +1,40 @@
+%% see red comment on page 14 of Paper3_commented.pdf:
+%% plots with comparisons between FD and GRW solutions show that by increasing the number of particles 
+%% the two solutions become identical in the limit of machine precission; this results invalidates the claim  
+%% that "the relationship between the number of particles and pressure head is neither smooth nor explicit"
+%% is a GROSS FALISIFICATION, used as principal motivation in all the tree papers of Song and Jiang. 
+
+close all
+
+load pGRWe3.mat
+pGRWe3=p;
+load pGRWe6.mat
+pGRWe6=p;
+load pGRWe10.mat
+pGRWe10=p;
+load pGRWe18.mat
+pGRWe18=p;
+load pGRWe24.mat
+pGRWe24=p;
+load pFD.mat
+pFD=p;
+
+figure; hold all;
+plot(pGRWe3,x,'rd','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+plot(pGRWe6,x,'b+','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+plot(pGRWe10,x,'m*','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+plot(pGRWe18,x,'gs','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+plot(pGRWe24,x,'yo','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+plot(pFD,x,'k.','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+ylabel('$z$','Interpreter','latex'); xlabel('$\psi(z)$','Interpreter','latex');
+legend('N=1e3','N=1e6','N=1e10','N=1e18','N=1e24','FD','Interpreter','latex'); box on;
+
+figure; hold all;
+plot(abs(pFD-pGRWe3)./abs(pFD),x,'rd','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+plot(abs(pFD-pGRWe6)./abs(pFD),x,'b+','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+plot(abs(pFD-pGRWe10)./abs(pFD),x,'m*','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+plot(abs(pFD-pGRWe18)./abs(pFD),x,'gs','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+plot(abs(pFD-pGRWe24)./abs(pFD),x,'k.','LineWidth',1.2,'MarkerFaceColor','yellow','MarkerSize',4) ;
+ylabel('$z$','Interpreter','latex'); xlabel('$(|\psi^{GRW}(z)-\psi^{FD}(z)|/|\psi^{FD}(z)|$','Interpreter','latex')
+legend('N=1e3','N=1e6','N=1e10','N=1e18','N=1e24','Interpreter','latex','FontSize',8,'location','best'); %  ,'Box','off'
+set(gca,'xscale','log'); box on; xlim([1e-20 1e5]); xlim([1e-20 1e10]); xticks([1e-20 1e-15 1e-10 1e-5 1 1e5 1e10]);
