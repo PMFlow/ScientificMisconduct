dc86d357c6914743f766457fa1f5c0dcdf0403c6
PMFlow <48481148+PMFlow@users.noreply.github.com>
2025-02-10 10:28:09 +0100

    Update DRW_Ref_27.m

--- 1_Song&Jiang_OklahomaStateUniversity/Song&Jiang_fabrication_and_falsification_Paper2/DRW_Ref_27.m
+++ 1_Song&Jiang_OklahomaStateUniversity/Song&Jiang_fabrication_and_falsification_Paper2/DRW_Ref_27.m
@@ -289,7 +289,7 @@ end
 %% original in GitHub/ DRW.m ===> Figs. 1b and 2b in Paper 2
 p1=psi(:,:,7);
 soil_moisture_content1=soil_moisture_content(:,:,7);
-%% by changing the position of the cross-section, DRW.m produces the Figs. 1a and 2a as well:
+%% by changing the position of the cross-section, DRW.m produces the Figs. 1a and 2a:
 % p1=psi(:,:,17); 
 % soil_moisture_content1=soil_moisture_content(:,:,17);
 %%
